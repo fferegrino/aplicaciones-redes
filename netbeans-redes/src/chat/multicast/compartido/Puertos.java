ac0ca8687083dea26248deb6c7062069255c6031
Antonio Feregrino <antonio@fferegrino.org>
2014-02-22 20:38:18 -0600

    Práctica 3
    Chat multicast

---
+++ netbeans-redes/src/chat/multicast/compartido/Puertos.java
@@ -0,0 +1,15 @@
+package chat.multicast.compartido;
+
+/**
+ *
+ * @author (at)fferegrino
+ */
+
+public class Puertos {
+
+
+public static final int SERVIDOR_MULTICAST = 4041;
+public static final int SERVIDOR_FLUJO = 4042;
+public static final int CLIENTE_MULTICAST = 4043;
+public static final int CLIENTE_FLUJO = 4044;
+}
