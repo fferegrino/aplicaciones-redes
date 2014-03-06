ec0408b5fe5a3cfb295b7653bf557ce357b46cd0
Antonio Feregrino <antonio@fferegrino.org>
2014-03-05 20:48:54 -0600

    ¡Carrito con imagenes!
    :)

--- netbeans-redes/src/carrito/cliente/Cliente.java
+++ netbeans-redes/src/carrito/cliente/Cliente.java
@@ -7,12 +7,17 @@ package carrito.cliente;
 import carrito.compartido.Interaccion;
 import carrito.compartido.Orden;
 import carrito.compartido.Producto;
+import chat.multicast.cliente.ClienteGUI;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,46 +29,14 @@ public class Cliente {
     public final static int COMPRA = 3;
     public final static int ACTUALIZAR_CARRITO = 2;
     public final static int SALIR = 4;
+    public final static int SOLICITAR_IMAGENES = 1;
+    static int bufferSize = 1024;
+    static byte[] buffer = new byte[bufferSize];
+    static String photoFolder = "C:\\carrito\\cliente\\pic\\";
 
     /**
      * @param args the command line arguments
      */
-//    public static void main(String[] args) throws Exception {
-//
-//        System.out.println("Bienvenido al carrito de compras.\nEstos son los productos que existen disponibles:");
-//        ArrayList<Producto> productos = solicitaProductos();
-//        imprimeProductos(productos);
-//        int opt;
-//
-//        do {
-//            System.out.println("Elija una opción:\n1. Ver carrito\t2. Actualizar carrito\t3. Realizar compra\t4. Salir");
-//            Scanner in = new Scanner(System.in);
-//            opt = in.nextInt();
-//            switch (opt) {
-//                case VER_CARRITO:
-//                    imprimeProductos(productos);
-//                    break;
-//                case ACTUALIZAR_CARRITO:
-//                    productos = solicitaProductos();
-//                    imprimeProductos(productos);
-//                    break;
-//                case COMPRA:
-//                    System.out.println("Ingrese el ID del producto elegido:\n");
-//                    int pid = in.nextInt();
-//                    System.out.println("Ingrese la cantidad deseada:\n");
-//                    int qty = in.nextInt();
-//                    if (realizaOrden(pid, qty) == Interaccion.TRANSACCION_CORRECTA) {
-//                        System.out.println("Transacción correcta!!");
-//                    }
-//                    break;
-//            }
-//        } while (opt != SALIR);
-//    }
-//    private static void imprimeProductos(ArrayList<Producto> productos) {
-//        for (int i = 0; i < productos.size(); i++) {
-//            System.out.println(productos.get(i));
-//        }
-//    }
     public ArrayList<Producto> solicitaProductos() throws Exception {
         ArrayList<Producto> res = new ArrayList<Producto>();
         Socket s = new Socket(InetAddress.getByName("localhost"), 4040);
@@ -77,6 +50,11 @@ public class Cliente {
         int cantidadProductos = is.readInt();
         while (cantidadProductos-- > 0) {
             res.add((Producto) is.readObject());
+        }
+        os.writeInt(SOLICITAR_IMAGENES);
+        os.flush();
+        if (SOLICITAR_IMAGENES == 1) {
+            leeImagenes(os, is);
         }
 
         os.close();
@@ -99,5 +77,49 @@ public class Cliente {
         is.close();
         s.close();
         return resultado;
+    }
+
+    private void leeImagenes(ObjectOutputStream os, ObjectInputStream is) {
+        try {
+            int totalArchivos = is.readInt(), resultadoLectura;
+            for (int i = 0; i < totalArchivos; i++) {
+                int longNombreArchivo = is.readInt();
+                byte[] nombreArchivo = new byte[longNombreArchivo];
+                // Leemos el nombre del archivo
+                is.read(nombreArchivo);
+                String nombre = new String(nombreArchivo);
+                // Leemos la longitud en bytes del archivo
+                long longArchivo = is.readLong();
+                // FileOutputStream para escribir el archivo
+                FileOutputStream fos = new FileOutputStream(photoFolder + nombre);
+                System.out.println("Archivo " + nombre + " " + longArchivo);
+                // Leemos la cantidad de bytes que se recibirán del servidor
+                resultadoLectura = is.readInt();
+                // Ciclo para leer del cliente y escribir al archivo
+                while (resultadoLectura >= 0) {
+                    System.out.println("\tBytes leídos " + resultadoLectura);
+                    // Leemos del socket los bytes
+                    try {
+                        is.read(buffer, 0, resultadoLectura);
+                        os.writeInt(1);
+                        os.flush();
+                    } catch (java.lang.ArrayIndexOutOfBoundsException ex) {
+                        os.writeInt(0);
+                        os.flush();
+                        break;
+                    }
+                    // Escribimos los bytes al archivo
+                    fos.write(buffer, 0, resultadoLectura);
+                    // Leemos la cantidad de bytes que se recibirán del servidor
+                    resultadoLectura = is.readInt();
+                }
+                System.out.println("Escritura terminada");
+                // Cerramos el FileOutputStream
+                fos.close();
+
+
+            }
+        } catch (IOException ex) {
+        }
     }
 }
--- netbeans-redes/src/carrito/servidor/Servidor.java
+++ netbeans-redes/src/carrito/servidor/Servidor.java
@@ -7,11 +7,16 @@ package carrito.servidor;
 import carrito.compartido.Interaccion;
 import carrito.compartido.Orden;
 import carrito.compartido.Producto;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,7 +25,10 @@ import java.util.ArrayList;
 public class Servidor {
 
     static String photoFolder = "C:\\carrito\\servidor\\pic\\";
-    
+    public final static int SOLICITAR_IMAGENES = 1;
+    static int bufferSize = 1024;
+    static byte[] buffer = new byte[bufferSize];
+
     /**
      * @param args the command line arguments
      */
@@ -31,7 +39,7 @@ public class Servidor {
 
 
         // <editor-fold defaultstate="collapsed" desc="Productos">
-        
+
         ArrayList<Producto> prods;
         prods = new ArrayList<Producto>();
         for (int x = 0; x < 7; x++) {
@@ -51,17 +59,22 @@ public class Servidor {
             ObjectOutputStream os = new ObjectOutputStream(s.getOutputStream());
             ObjectInputStream is = new ObjectInputStream(s.getInputStream());
             os.flush();
-            
+
             System.out.println("Cliente aceptado " + s.getInetAddress());
             option = is.readInt();
-            
-            
+
+
             switch (option) {
                 case Interaccion.SOLICITUD_CARRITO:
                     System.out.println("Solicitud carrito");
                     os.writeInt(prods.size());
                     for (int i = 0; i < prods.size(); i++) {
                         os.writeObject(prods.get(i));
+                    }
+                    os.flush();
+                    if (is.readInt() == SOLICITAR_IMAGENES) {
+                        System.out.println("Cliente solicita imagenes");
+                        escribeImagenes(os, is);
                     }
                     break;
                 case Interaccion.CONFIRMAR_COMPRA:
@@ -71,12 +84,65 @@ public class Servidor {
                     os.writeInt(Interaccion.TRANSACCION_CORRECTA);
                     break;
             }
-            
+
             System.out.println("Solicitud de " + s.getInetAddress() + " terminada");
-            
+
             os.close();
             is.close();
             s.close();
+        }
+    }
+
+    private static void escribeImagenes(ObjectOutputStream os, ObjectInputStream is) {
+
+        int i, resultadoLectura;
+        File picFolder = new File(photoFolder);
+        File[] files = picFolder.listFiles();
+        try {
+            os.writeInt(files.length);
+
+            for (i = 0; i < files.length; i++) {
+                // Conversión del nombre del archivo a bytes
+                byte[] nombreArchivo = files[i].getName().getBytes();
+                // Enviamos la longitud del nombre del archivo
+                os.writeInt(nombreArchivo.length);
+                    os.flush();
+                // Enviamos el nombre del archivo
+                os.write(nombreArchivo);
+                    os.flush();
+                // Enviamos el tamaño en bytes del archivo
+                os.writeLong(files[i].length());
+                    os.flush();
+                // FileInputStream para leer del archivo
+                FileInputStream fis = new FileInputStream(files[i]);
+                // Leemos al buffer de bytes
+                resultadoLectura = fis.read(buffer);
+                // Escritura de los bytes leídos
+                os.writeInt(resultadoLectura);
+                    os.flush();
+                // Ciclo para leer del archivo:
+                while (resultadoLectura >= 0) {
+                    // Escribimos al socket los bytes del archivo
+                    os.write(buffer, 0, resultadoLectura);
+                    os.flush();
+                    int codigo = is.readInt();
+
+                    if (codigo == 0) {
+                        System.out.println("\tError");
+                        break;
+                    }
+                    // Volvemos a leer
+                    resultadoLectura = fis.read(buffer);
+                    // Escritura de los bytes leídos
+                    os.writeInt(resultadoLectura);
+                    os.flush();
+                }
+                // Cerramos el FileInputStream
+                fis.close();
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
